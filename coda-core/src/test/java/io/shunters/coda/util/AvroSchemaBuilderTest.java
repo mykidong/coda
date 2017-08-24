4f46f74557976b597e2bef97de6747af56ed555d
Kidong Lee <mykidong@gmail.com>
2017-08-24 21:26:41 +0900

    avro schema builder added to create avro schema instances from the classpath avsc files

---
+++ coda-core/src/test/java/io/shunters/coda/util/AvroSchemaBuilderTest.java
@@ -0,0 +1,57 @@
+package io.shunters.coda.util;
+
+import org.apache.avro.Schema;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mykidong on 2017-08-24.
+ */
+public class AvroSchemaBuilderTest {
+
+    private static Logger log;
+
+    @Before
+    public void init() throws Exception {
+        java.net.URL url = new AvroSchemaBuilderTest().getClass().getResource("/log4j-test.xml");
+        System.out.println("log4j url: " + url.toString());
+
+        DOMConfigurator.configure(url);
+
+        log = LoggerFactory.getLogger(AvroSchemaBuilder.class);
+    }
+
+    @Test
+    public void loadSchemas() throws Exception
+    {
+        List<String> pathList = new ArrayList<>();
+        pathList.add("/META-INF/avro/request-header.avsc");
+        pathList.add("/META-INF/avro/record-header.avsc");
+        pathList.add("/META-INF/avro/record.avsc");
+        pathList.add("/META-INF/avro/records.avsc");
+        pathList.add("/META-INF/avro/produce-request.avsc");
+
+        AvroSchemaBuilder avroSchemaBuilder = new AvroSchemaBuilder((String[])pathList.toArray(new String[0]));
+
+        String schemaKey = "io.shunters.coda.avro.api.ProduceRequest";
+        Schema schema = avroSchemaBuilder.getSchema(schemaKey);
+        System.out.println("schema key: [" + schemaKey + "]\n" + schema.toString(true));
+    }
+
+    @Test
+    public void loadSchemasWithPathDir() throws Exception
+    {
+        String pathDir = "/META-INF/avro";
+
+        AvroSchemaBuilder avroSchemaBuilder = new AvroSchemaBuilder(pathDir);
+        String schemaKey = "io.shunters.coda.avro.api.ProduceRequest";
+        Schema schema = avroSchemaBuilder.getSchema(schemaKey);
+        log.info("schema key: [" + schemaKey + "]\n" + schema.toString(true));
+    }
+}
